4d9d0cd28c58e79e50726ccf5c0be4b055a84309
Roberto <roberto.i12@icloud.com>
2018-05-14 12:32:10 +0200

    Revert "xd lol"
    This reverts commit 6351711e090ee71cb1827efc886af74e4b25c11f.

--- tetris-esqueleto.s
+++ tetris-esqueleto.s
@@ -1,7 +1,7 @@
-# Versión incompleta del tetris
+# Versión incompleta del tetris 
 # Sincronizada con tetris.s:r2916
-
-	.data
+        
+	.data	
 
 cadena_puntos:
     .space 256
@@ -113,14 +113,14 @@ end0:
 end1:
     	.asciiz     "  GAME OVER  \n"
 end2:
-    	.asciiz     "-------------\n"
+    	.asciiz     "-------------\n"   	    	
 
 
-	.text
+	.text	
 
 imagen_pixel_addr:			# ($a0, $a1, $a2) = (imagen, x, y)
 					# pixel_addr = &data + y*ancho + x
-    	lw	$t1, 0($a0)		# $a0 = dirección de la imagen
+    	lw	$t1, 0($a0)		# $a0 = dirección de la imagen 
 					# $t1 ← ancho
     	mul	$t1, $t1, $a2		# $a2 * ancho
     	addu	$t1, $t1, $a1		# $a2 * ancho + $a1
@@ -145,18 +145,18 @@ imagen_set_pixel:	# ($a0, $a1, $a2, $a3) = (*img, x, y, color)
 	addiu	$sp, $sp, -8
 	sw 	$ra, 4($sp)		# guardamos $ra porque haremos un jal
 	sw	$s0, 0($sp)		# guardamos $s0 para utilizarlo luego
-
+	
 	move 	$s0, $a3 		# guardamos $a3 en $s0 para asegurarnos de que no se pierde $s3 en otra función
 	jal 	imagen_pixel_addr	# saltamos a imagen_pixel_addr
 	sb 	$s0, 0($v0)		# guardamos "color" ($s0) en la posición devuelta por imagen_pixel_addr ($v0)
-
+	
 	lw 	$s0, 0($sp)		# restauramos $s0
 	lw	$ra, 4($sp)		# restauramos $ra
 	addiu	$sp, $sp, 8		# liberamos el espacio de la pila
-	jr 	$ra
-
+	jr 	$ra				
+	
 	# si no funciona comprobar las cosas que guarda y restaura de la pila
-
+	
 imagen_clean:		# ($a0, $a1) = (*img, fondo)
 	# void imagen_clean(Imagen *img, Pixel fondo) {
  	#    for (int y = 0; y < img->alto; ++y) {
@@ -164,47 +164,47 @@ imagen_clean:		# ($a0, $a1) = (*img, fondo)
   	#           imagen_set_pixel(*img, x, y, fondo);
  	#       }
  	#    }
-	# }
-
+	# } 
+	
 	addiu	$sp, $sp, -28
 	sw	$ra, 24($sp)
-	sw	$s0, 20($sp)
+	sw	$s0, 20($sp)		
 	sw	$s1, 16($sp)
 	sw	$s2, 12($sp)
 	sw	$s3, 8($sp)
 	sw	$s4, 4($sp)
 	sw	$s5, 0($sp)
-
+	
 	move	$s2, $a0
 	move	$s3, $a1
 	lw	$s4, 4($s2)		# carga img->alto en $s4
 	lw	$s5, 0($s2)		# carga img->ancho en $s5
-
+	
 	# for (int y = 0; y < img->alto; ++y) {
 	li 	$s0, 0			# int y = 0
 B0_1:	bge  	$s0, $s4, B0_2		# si y >= img->alto salta el bucle
-
+	
 	# for (int x = 0; x < img->ancho; ++x)
 	li	$s1, 0			# int x = 0
 B0_3:	bge	$s1, $s5, B0_4		# si x >= img->ancho salta el bucle
 
 	# imagen_set_pixel(*img, x, y, fondo)
-
+	
 	move	$a3, $a1
 	move	$a1, $s1
 	move	$a2, $s0
 	jal 	imagen_set_pixel
 	move 	$a0, $s2
 	move	$a1, $s3
-
+	
 	addiu	$s1, $s1, 1		# ++x
 	j	B0_3			# sigue el bucle
 	# }
-
+	
 B0_4:	addiu	$s0, $s0, 1		# ++y
 	j	B0_1
 	# }
-
+	
 B0_2:	lw	$s5, 0($sp)
 	lw	$s4, 4($sp)
 	lw	$s3, 8($sp)
@@ -212,54 +212,54 @@ B0_2:	lw	$s5, 0($sp)
 	lw 	$s1, 16($sp)
 	lw	$s0, 20($sp)
 	lw	$ra, 24($sp)
-	addiu	$sp, $sp, 28
+	addiu	$sp, $sp, 28	
      	jr $ra
-
-  #eres mas tonto que mis cojones   	
+     	
+     	
 game_over:				# CONTINUAR
 
-	lb	$t0, acabar_partida
-
+	lb	$t0, acabar_partida 	
+	
 	# if ($t0 == 1) print ( 'GAME OVER' )
 	beqz 	$t0, B7_01		# Salta el bucle si no ha acabado la partida
-
+	
 	la	$a0, pantalla
-	la	$a1, end0
+	la	$a1, end0	
 	li	$a2, 0
 	li	$a3, 4
 	jal	imagen_dibuja_cadena	# imagenDibujaCadena(*img, cadena, x, y)
-
+	
 	la	$a0, pantalla
-	la	$a1, end1
+	la	$a1, end1	
 	li	$a2, 0
 	li	$a3, 5
 	jal	imagen_dibuja_cadena	# imagenDibujaCadena(*img, cadena, x, y)
-
+	
 	la	$a0, pantalla
-	la	$a1, end2
+	la	$a1, end2	
 	li	$a2, 0
 	li	$a3, 6
 	jal	imagen_dibuja_cadena	# imagenDibujaCadena(*img, cadena, x, y)
-B7_01:
+B7_01:	
 	jr $ra
 
-
+        
 imagen_init:
 	# void imagen_init(Imagen *img, int ancho, int alto, Pixel fondo) {
   	# 	img->ancho = ancho;
   	# 	img->alto = alto;
   	# 	imagen_clean(*img, fondo);
 	# }
-
+	
 	addiu 	$sp, $sp, -4		# Hacemos hueco en la pila para guardar $ra
 	sw 	$ra, 0($sp)
-
+	
 	sw	$a1, 0($a0)		# img->ancho = ancho
 	sw	$a2, 4($a0)		# img->alto = alto
-
+	
 	move 	$a1, $a3		# Preparamos los parámetros para llamar a imagen_clean
-	jal 	imagen_clean
-
+	jal 	imagen_clean		
+	
 	lw	$ra, 0($sp)		# Restauramos la pila
 	addiu	$sp, $sp, 4
 	jr $ra
@@ -278,7 +278,7 @@ imagen_copy:
 
 	addiu	$sp, $sp, -28
 	sw	$ra, 24($sp)
-	sw	$s0, 20($sp)
+	sw	$s0, 20($sp)		
 	sw	$s1, 16($sp)
 	sw	$s2, 12($sp)
 	sw	$s3, 8($sp)
@@ -290,7 +290,7 @@ imagen_copy:
 
 	lw	$s0, 0($s5)		# Cargamos src->ancho en $s0
 	sw	$s0, 0($s4)		# Guardamos $s0 en dst->ancho
-
+	
 	lw	$s1, 4($s5)		# Cargamos src->alto en $s1
 	sw 	$s1, 4($s4)		# Guardamos $s1 en dst->alto
 
@@ -308,22 +308,22 @@ B1_3:	bge	$s3, $s0, B1_4		# si x >= src->ancho salta el bucle
 	move	$a1, $s3
 	move	$a2, $s2
 	jal 	imagen_get_pixel
-	move	$a3, $v0		# guardamos p en $a3 para metérselo como parámetro a imagen_set_pixel
+	move	$a3, $v0		# guardamos p en $a3 para metérselo como parámetro a imagen_set_pixel	
 	# imagen_set_pixel(dst, x, y, p);
 	move	$a0, $s4
 	move	$a1, $s3
 	move	$a2, $s2
 	jal imagen_set_pixel
-
+	
 	addiu	$s3, $s3, 1		# ++x
 	j	B1_3			# sigue el bucle
 	# }
-
+	
 B1_4:	addiu	$s2, $s2, 1		# ++y
 	j	B1_1			# sigue el bucle
 	# }
-
-B1_2:
+	
+B1_2:	
 	lw	$s5, 0($sp)
 	lw	$s4, 4($sp)
 	lw	$s3, 8($sp)
@@ -331,8 +331,8 @@ B1_2:
 	lw 	$s1, 16($sp)
 	lw	$s0, 20($sp)
 	lw	$ra, 24($sp)
-	addiu	$sp, $sp, 28
-     	jr $ra
+	addiu	$sp, $sp, 28	
+     	jr $ra	
 
 
 
@@ -414,7 +414,7 @@ imagen_dibuja_imagen:
 	move 	$s3, $a3		# $s3 = dst_y
 	lw	$s4, 4($s1)		# $s4 = src->alto
 	lw	$s5, 0($s1)		# $s5 = src->ancho
-
+	
 	# for (int y = 0; y < src->alto; ++y) {
 	li 	$s6, 0			# int y = 0
 B2_1:	bge	$s6, $s4, B2_2		# si y >= src->alto salta el bucle
@@ -422,19 +422,19 @@ B2_1:	bge	$s6, $s4, B2_2		# si y >= src->alto salta el bucle
 	li	$s7, 0			# int x = 0
 
 B2_3:	bge	$s7, $s5, B2_4		# si x >= src->ancho salta el bucle
-
+	
 	# Pixel p = imagen_get_pixel(src, x, y);
 	move 	$a0, $s1
 	move	$a1, $s7
 	move	$a2, $s6
 	jal 	imagen_get_pixel
-
+	
 	# if (p != PIXEL_VACIO) {
 	beqz   	$v0, B2_31
-
+	
 	# imagen_set_pixel(*dst, dst_x + x, dst_y + y, p);
-	move 	$a0, $s0		# $a0 = dst
-	add	$a1, $s2, $s7		# $a1 = dst_x + x
+	move 	$a0, $s0		# $a0 = dst	
+	add	$a1, $s2, $s7		# $a1 = dst_x + x	
 	add	$a2, $s3, $s6		# $a1 = dst_y + y
 	move	$a3, $v0		# $s3 = p
 	jal 	imagen_set_pixel
@@ -443,7 +443,7 @@ B2_3:	bge	$s7, $s5, B2_4		# si x >= src->ancho salta el bucle
 B2_31:	addiu	$s7, $s7, 1		# ++x
 	j	B2_3			# sigue el bucle
 	# }
-
+	
 B2_4:	addiu	$s6, $s6, 1		# ++y
 	j	B2_1			# sigue el bucle
 	# }
@@ -462,7 +462,7 @@ B2_2:
 	jr	$ra
 
 imagen_dibuja_imagen_rotada:
-
+	
 	# void imagen_dibuja_imagen_rotada(Imagen *dst, Imagen *src, int dst_x, int dst_y) {
   	#    for (int y = 0; y < src->alto; ++y) {
     	#       for (int x = 0; x < src->ancho; ++x) {
@@ -491,7 +491,7 @@ imagen_dibuja_imagen_rotada:
 	move 	$s3, $a3		# $s3 = dst_y
 	lw	$s4, 4($s1)		# $s4 = src->alto
 	lw	$s5, 0($s1)		# $s5 = src->ancho
-
+	
 	# for (int y = 0; y < src->alto; ++y) {
 	li 	$s6, 0			# int y = 0
 B3_1:	bge	$s6, $s4, B3_2		# si y >= src->alto salta el bucle
@@ -499,19 +499,19 @@ B3_1:	bge	$s6, $s4, B3_2		# si y >= src->alto salta el bucle
 	li	$s7, 0			# int x = 0
 
 B3_3:	bge	$s7, $s5, B3_4		# si x >= src->ancho salta el bucle
-
+	
 	# Pixel p = imagen_get_pixel(src, x, y);
 	move 	$a0, $s1
 	move	$a1, $s7
 	move	$a2, $s6
 	jal 	imagen_get_pixel
-
+	
 	# if (p != PIXEL_VACIO) {
 	beqz   	$v0, B3_31
-
-
+	
+	
 	# imagen_set_pixel(dst, dst_x + src->alto - 1 - y, dst_y + x, p);
-	move 	$a0, $s0		# $a0 = dst
+	move 	$a0, $s0		# $a0 = dst	
 	add	$a1, $s2, $s4		# $a1 = dst_x + src->alto
 	subi 	$a1, $a1, 1 		# $a1 = $a1 - 1
 	sub 	$a1, $a1, $s6		# $a1 = $a1 - y
@@ -519,12 +519,12 @@ B3_3:	bge	$s7, $s5, B3_4		# si x >= src->ancho salta el bucle
 	move	$a3, $v0		# $a3 = p
 	jal imagen_set_pixel
 	# }
-
+	
 
 B3_31:	addiu	$s7, $s7, 1		# ++x
 	j	B3_3			# sigue el bucle
 	# }
-
+	
 B3_4:	addiu	$s6, $s6, 1		# ++y
 	j	B3_1			# sigue el bucle
 	# }
@@ -540,7 +540,7 @@ B3_2:
 	lw	$s7, 28($sp)
 	lw	$ra, 32($sp)
 	addiu	$sp, $sp, 36
-	jr	$ra
+	jr	$ra	
 
 
 pieza_aleatoria:
@@ -584,7 +584,7 @@ B10_2:	lw	$t1, 4($s2)		# campo->alto
         addiu	$s1, $s1, 1		# ++y
         j       B10_2
         # } // for y
-	# for (int x = 0; x < campo->ancho + 2; ++x) {
+	# for (int x = 0; x < campo->ancho + 2; ++x) { 
 B10_3:	li	$s1, 0			# x = 0
 B10_5:  lw	$t1, 0($s2)		# campo->ancho
         addiu   $t1, $t1, 2             # campo->ancho + 2
@@ -611,28 +611,28 @@ B10_6:	la	$a0, pantalla
 	addiu	$a3, $t1, 2		# pieza_actual_y + pos_campo_y
 	jal	imagen_dibuja_imagen	# imagen_dibuja_imagen(pantalla, pieza_actual, pieza_actual_x + pos_campo_x, pieza_actual_y + pos_campo_y)
 	jal	clear_screen		# clear_screen()
-
+	
 	la	$a0, pantalla
-	la	$a1, str003
+	la	$a1, str003	
 	li	$a2, 0
 	li	$a3, 0
 	jal	imagen_dibuja_cadena	# imagenDibujaCadena(*img, cadena, x, y)
-
+	
 	lw	$a0, puntuacion_actual
-	li	$a1, 10
+	li	$a1, 10			
 	la	$a2, cadena_puntos	# dirección donde guardaremos la cadena
 	jal	integer_to_string_v4
-
+	
 	la	$a0, pantalla
 	la	$a1, cadena_puntos
 	li	$a2, 12
 	li	$a3, 0
 	jal 	imagen_dibuja_cadena
-
-
-
-
-
+	
+	
+	
+	
+	
 	la	$a0, pantalla
 	jal	imagen_print		# imagen_print(pantalla)
 	lw	$s1, 0($sp)
@@ -648,25 +648,25 @@ nueva_pieza_actual:
   	#    pieza_actual_x = 8;
   	#    pieza_actual_y = 0;
 	# }
-
-	addiu	$sp, $sp, -4
+	
+	addiu	$sp, $sp, -4		
 	sw	$ra, 0($sp)
-
+	
 	jal 	pieza_aleatoria		# $v0 == Imagen *elegida
-
+	
 	la	$a0, pieza_actual	# Cargamos la dirección de pieza_actual en $a0 para llamar a imagen_copy
 	move 	$a1, $v0		# Cargamos *elegida en $a1 para llamar a imagen_copy
-	jal	imagen_copy
-
+	jal	imagen_copy		
+	
 	li	$t0, 8
 	sw 	$t0, pieza_actual_x	# pieza_actual_x = 8;
 	li	$t1, 0
 	sw 	$t1, pieza_actual_y	# pieza_actual_y = 0;
-
+	
 	lw	$ra, 0($sp)
 	addiu	$sp, $sp, 4
 	jr	$ra
-
+	
 
 probar_pieza:				# ($a0, $a1, $a2) = (pieza, x, y)
 	addiu	$sp, $sp, -32
@@ -741,20 +741,20 @@ intentar_movimiento:
  	# }
   	# return false;
 	# }
-
+	
 	addiu	$sp, $sp, -12
 	sw	$ra, 8($sp)
 	sw	$s0, 4($sp)
 	sw	$s1, 0($sp)
-
+	
 	move	$s0, $a0		# $s0 = x
 	move	$s1, $a1		# $s1 = y
-
+	
 	la	$a0, pieza_actual
 	move	$a1, $s0
 	move	$a2, $s1
 	jal 	probar_pieza
-
+	
 	# if (probar_pieza(pieza_actual, x, y)) {
 	beqz 	$v0, B13_1		# si no se cumple la condición del if, saltamos a B13_1
 	sw	$s0, pieza_actual_x	# pieza_actual_x = x
@@ -762,9 +762,9 @@ intentar_movimiento:
 	li	$v0, 1			# return 1
 	j 	B13_2
 	# }
-
+	
 B13_1:  li	$v0, 0			# return 0
-
+	
 B13_2:	lw	$s1, 0($sp)
 	lw	$s0, 4($sp)
 	lw	$ra, 8($sp)
@@ -778,29 +778,29 @@ bajar_pieza_actual:
     	#       nueva_pieza_actual();
   	#    }
 	# }
-
+	
 	addiu	$sp, $sp, -4
-	sw	$ra, 0($sp)
-
+	sw	$ra, 0($sp)	
+	
 	lw	$a0, pieza_actual_x	# perparamos los parámetros para llamar a intentar_movimiento
 	lw	$a1, pieza_actual_y
 	addi 	$a1, $a1, 1
-
+	
 	jal intentar_movimiento
 	# if (!intentar_movimiento(pieza_actual_x, pieza_actual_y + 1)) {
 	bnez  	$v0, B14_1		# si no se cumple la condición salta el bucle
-
+	
 	la	$a0, campo
 	la	$a1, pieza_actual
 	lw	$a2, pieza_actual_x
 	lw	$a3, pieza_actual_y
 	jal 	imagen_dibuja_imagen
 	jal	nueva_pieza_actual
-
+	
 	lw	$t9, puntuacion_actual
 	addi	$t9, $t9, 1
 	sw	$t9, puntuacion_actual
-
+	
 	# }
 B14_1:	lw	$ra, 0($sp)
 	addiu 	$sp, $sp, 4
@@ -822,27 +822,27 @@ intentar_rotar_pieza_actual:
 
 
 	la	$s0, imagen_auxiliar	# Imagen *pieza_rotada = imagen_auxiliar
-
+	
 	move	$a0, $s0
 	la 	$t0, pieza_actual	# cargamos pieza_actual en $t0 para sacar pieza_actual->alto
-	lw	$a1, 4($t0)
+	lw	$a1, 4($t0)		
 	lw	$a2, 0($t0)
 	move	$a3, $zero
 	jal 	imagen_init
-
+	
 	move	$a0, $s0
 	la	$a1, pieza_actual
 	move	$a2, $zero
 	move	$a3, $zero
 	jal 	imagen_dibuja_imagen_rotada
-
+	
 	move	$a0, $s0
 	lw	$a1, pieza_actual_x
 	lw	$a2, pieza_actual_y
 	jal	probar_pieza
 	# if (probar_pieza(pieza_rotada, pieza_actual_x, pieza_actual_y)) {
 	beqz	$v0, B15_1
-
+	
 	la	$a0, pieza_actual
 	move	$a1, $s0
 	jal	imagen_copy		# imagen_copy(pieza_actual, pieza_rotada)
@@ -904,9 +904,9 @@ procesar_entrada:
 	sw	$s0, 0($sp)
 	jal	keyio_poll_key
 	move	$s0, $v0		# int c = keyio_poll_key()
-        # for (int i = 0; i < sizeof(opciones) / sizeof(opciones[0]); ++i) {
+        # for (int i = 0; i < sizeof(opciones) / sizeof(opciones[0]); ++i) { 
 	li	$s1, 0			# i = 0, $s1 = i * sizeof(opciones[0]) // = i * 8
-	la	$s3, procesar_entrada.opciones
+	la	$s3, procesar_entrada.opciones	
 	li	$s4, 40			# sizeof(opciones) // == 5 * sizeof(opciones[0]) == 5 * 8
 B21_1:	addu	$t1, $s3, $s1		# procesar_entrada.opciones + i*8
 	lb	$t2, 0($t1)		# opciones[i].tecla
@@ -926,7 +926,7 @@ B21_3:	addiu	$s1, $s1, 8		# ++i, $s1 += 8
 	jr	$ra
 
 jugar_partida:
-	addiu	$sp, $sp, -12
+	addiu	$sp, $sp, -12	
 	sw	$ra, 8($sp)
 	sw	$s1, 4($sp)
 	sw	$s0, 0($sp)
@@ -946,7 +946,7 @@ jugar_partida:
 	move	$s0, $v0		# Hora antes = get_time()
 	jal	actualizar_pantalla	# actualizar_pantalla()
 	j	B22_2
-        # while (!acabar_partida) {
+        # while (!acabar_partida) { 
 B22_2:	lbu	$t1, acabar_partida
 	bnez	$t1, B22_5		# if (acabar_partida != 0) sale del bucle
 	jal	procesar_entrada	# procesar_entrada()
@@ -958,7 +958,7 @@ B22_1:	jal	bajar_pieza_actual	# bajar_pieza_actual()
 	jal	actualizar_pantalla	# actualizar_pantalla()
 	move	$s0, $s1		# antes = ahora
         j	B22_2			# siguiente iteración
-       	# }
+       	# } 
 B22_5:	lw	$s0, 0($sp)
 	lw	$s1, 4($sp)
 	lw	$ra, 8($sp)
@@ -966,7 +966,7 @@ B22_5:	lw	$s0, 0($sp)
 	jr	$ra
 
 	.globl	main
-main:					# ($a0, $a1) = (argc, argv)
+main:					# ($a0, $a1) = (argc, argv) 
 	addiu	$sp, $sp, -4
 	sw	$ra, 0($sp)
 B23_2:	jal	clear_screen		# clear_screen()
@@ -996,39 +996,39 @@ B23_5:	la	$a0, str002
 
 print_character:
 	li	$v0, 11
-	syscall
+	syscall	
 	jr	$ra
 
 print_string:
 	li	$v0, 4
-	syscall
+	syscall	
 	jr	$ra
 
 get_time:
 	li	$v0, 30
-	syscall
+	syscall	
 	move	$v0, $a0
 	move	$v1, $a1
 	jr	$ra
 
 read_character:
 	li	$v0, 12
-	syscall
+	syscall	
 	jr	$ra
 
 clear_screen:
 	li	$v0, 39
-	syscall
+	syscall	
 	jr	$ra
 
 mips_exit:
 	li	$v0, 17
-	syscall
+	syscall	
 	jr	$ra
 
 random_int_range:
 	li	$v0, 42
-	syscall
+	syscall	
 	move	$v0, $a0
 	jr	$ra
 
@@ -1042,32 +1042,32 @@ keyio_poll_key_return:
 	jr	$ra
 
 integer_to_string_v4:			# ($a0, $a1, $a2) = (n, base, buf)
-
+	
 	move    $t0, $a2		# char *p = buff
 	beqz 	$a0, B4_22		# Si es 0 pasa directamente a la etiqueda B4_22
 	li    $t5, 10
 	# for (int i = |n|; i > 0; i = i / base) {
         abs	$t1, $a0		# int i = |n|
-
+        
 B4_1:   blez	$t1, B4_2		# si i <= 0 salta el bucle
 	div	$t1, $a1		# i / base
 	mflo	$t1			# i = i / base
-	mfhi	$t2			# d = i % base
+	mfhi	$t2			# d = i % base  
   	blt    $t2, $t5, B4_11  # if d < 10 then B4_11
   	bge    $t2, $t5, B4_12  # if d >= 10 then B4_12
 B4_11:  addiu	$t2, $t2, '0'		# d + '0'
-        sb	$t2, 0($t0)		# *p = $t2
+        sb	$t2, 0($t0)		# *p = $t2 
         addiu	$t0, $t0, 1		# ++p
         j	B4_1			# sigue el bucle
-        # }
-       	# Los valores > 9 se pasan a su correspondiente valor A, B...
+        # }     
+       	# Los valores > 9 se pasan a su correspondiente valor A, B...  
 B4_12: 	sub 	$t2, $t2, $t5		# d - 10
-	addiu	$t2, $t2, 'A'		# d + 'A'
-        sb	$t2, 0($t0)		# *p = $t2
+	addiu	$t2, $t2, 'A'		# d + 'A'  
+        sb	$t2, 0($t0)		# *p = $t2 
         addiu	$t0, $t0, 1		# ++p
         j	B4_1			# sigue el bucle
-        # }
-
+        # }    
+        
 B4_2: 	bgtz 	$a0, B4_21  		# si n > 0 salta a B4_21
 
 	li    	$t4, '-'    		# cargamos en $t4 '-'
@@ -1076,17 +1076,17 @@ B4_2: 	bgtz 	$a0, B4_21  		# si n > 0 salta a B4_21
 
 B4_21: 	sb	$zero, 0($t0)		# *p = '\0'
 
-	# Precondición bucle ordenar
+	# Precondición bucle ordenar	
 	# $t0 es el puntero de la última posición y $a2 la posición del primer número
 	subi 	$t0, $t0, 1 		# Coloca el puntero $t0 una posición anterior
 	move 	$t1, $a2		# Usamos $t1 en vez de $a2
-
+	
 	# Bucle para ordenar. Los dos punteros son $t1 (L) y $t0 (R)
 B4_A:	bge 	$t1, $t0, B4_4		# Termina el bucle cuando $t1 sea mayor o igual que $t0
 	lb 	$t2, 0($t1)		# Guarda el contenido de ($t1) en $t2
 	lb 	$t3, 0($t0)		# Guarda el contenido de ($t0) en $t3
 	sb 	$t2, 0($t0)		# Guarda el contenido de $t2 en ($t0)
-	sb 	$t3, 0($t1)		# Guarda el contenido de $t3 en ($t1)
+	sb 	$t3, 0($t1)		# Guarda el contenido de $t3 en ($t1)	
 	addiu	$t1, $t1, 1		# Incrementa el puntero $t1
 	subiu	$t0, $t0, 1		# Decrementa el puntero $t0
 	j 	B4_A			# Repite el bucle
@@ -1108,16 +1108,16 @@ imagen_dibuja_cadena:        		#($a0,$a1, $a2,$a3) = (pantalla, dircadena, pos x
 	sw	$s3, 8($sp)
 	sw	$s4, 4($sp)
 	sw	$ra, 0($sp)
-
+	
 	move	$s0, $a0		# $s0 = pantalla	= *img
 	move	$s1, $a1		# $s1 = dircadena
 	move	$s2, $a2		# $s2 = pos x
 	move	$s3, $a3		# $s3 = pos y
-
+	
 B5_1:	lb	$s4, 0($s1)		# Cargamos el carácter de la cadena ($s1) en $s4
 	beqz	$s4, B5_2		# Si el carácter es \0 termina el bucle
 
-	move	$a0, $s0		# $a0 = pantalla
+	move	$a0, $s0		# $a0 = pantalla 	
 	move 	$a1, $s2		# $a1 = $s2
 	move	$a2, $s3		# $a2 = $s3
 	move	$a3, $s4		# $a3 = Pixel (carácter)
@@ -1126,8 +1126,8 @@ B5_1:	lb	$s4, 0($s1)		# Cargamos el carácter de la cadena ($s1) en $s4
 	addi 	$s1, $s1, 1		# Siguiente pixel
 	addi 	$s2, $s2, 1		# x++
 	j	B5_1
-
-B5_2:	lw	$s0, 20($sp)
+	
+B5_2:	lw	$s0, 20($sp) 
 	lw	$s1, 16($sp)
 	lw	$s2, 12($sp)
 	lw	$s3, 8($sp)
@@ -1135,3 +1135,16 @@ B5_2:	lw	$s0, 20($sp)
 	lw	$ra, 0($sp)
 	addi	$sp, $sp, 24
 	jr	$ra
+
+
+
+
+
+
+
+
+
+
+
+
+
